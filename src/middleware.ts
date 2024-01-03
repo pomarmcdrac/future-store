760bf5f00c482697d07d4652b5b67b9a2bf10dbb
Omar Morales <pomaral@live.com.mx>
2024-01-03 17:57:49 -0600

    Avances del curso con chat

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,19 @@
+import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
+export const config = {
+  matcher: [
+    '/login/:paths*',
+    '/signup/:path*',
+  ]
+}
+
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL('/home', request.url))
+  const cookiesStore = cookies()
+  const accesstoken = cookiesStore.get('token')?.value
+  if (accesstoken) {
+    return NextResponse.redirect(new URL('/store', request.url))
+  }
 }
  
