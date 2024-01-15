095d625feb912e4fd00e210880ca74696807dd22
Omar Morales <pomaral@live.com.mx>
2024-01-15 12:10:44 -0600

    Agrego metadata

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,4 @@
 import { MainProducts } from "app/components/home/MainProducts"
-import { Metadata } from "next"
-
-export const metadata: Metadata = {
-  title: "✨ Future World",
-  description: "Welcome to the future world, an ecommerce from other century",
-  keywords: ["ecommerce", "future", "world", "Technology"],
-}
 
 export default function Home() {
   return (
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,19 @@
 import { Header } from 'app/components/shared/Header'
 import { Footer } from 'app/components/shared/Footer'
 import { Roboto } from 'next/font/google'
+import { Metadata } from 'next'
 import 'app/sass/globals.sass'
 
 const roboto = Roboto({
   weight: ["100", "300", "500", "700"],
   subsets: ["latin"],
 })
+
+export const metadata: Metadata = {
+  title: "✨ Future World",
+  description: "Welcome to the future world, an ecommerce from other century",
+  keywords: ["ecommerce", "future", "world", "Technology"],
+}
 
 export default function RootLayout({
   children,
